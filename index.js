bfd2493e3a61b2815e460ca419574679596ac3eb
tarik-84 <niceloveman200@yahoo.com>
2020-01-22 19:25:36 -0500

    new fills

--- index.js
+++ index.js
@@ -39,9 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = []
 }
+Person.prototype.eat = function (someFood) {
+  
+  if(this.stomach.length < 10){
+    this.stomach.push(someFood)
+  }
+};
+Person.prototype.poop = function () {
+  this.stomach = []
+};
+Person.prototype.toString = function() {
+  return this.name + this.age
+};
+
 
 /*
   TASK 2
@@ -57,9 +72,22 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+Car.prototype.fill = function (gallons){
+    this.tank += gallons; 
+};
+Car.prototype.drive = function (distance){
+     this.odometer += distance;
+     this.milesPerGallon += this.tank;
+     if(this.tank === 0){
+       return `I ran out of fuel at ${distance}`
+     }
+};
 
 /*
   TASK 3
@@ -68,18 +96,22 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  Baby.prototype = Object.create(Person.prototype); 
+  this.favoriteToy = favoriteToy 
+  Baby.prototype.play = function () {
+    return `playing with ${this.favoriteToy}`
+};
 }
-
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Is the function called by new?
+  2. Is the function called by call(), apply(), or bind()?
+  3. Is the function called as a method, ie: obj.func()?
+  4. Is the function called in the global scope?
 */
 
 
